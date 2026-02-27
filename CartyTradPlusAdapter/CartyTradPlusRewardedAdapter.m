0932e4193548ffe9ef23101699ad1ef429801213
anplex <ssp_tech@carty.io>
2026-02-27 10:49:31 +0800

    update

--- CartyTradPlusAdapter/CartyTradPlusRewardedAdapter.m
+++ CartyTradPlusAdapter/CartyTradPlusRewardedAdapter.m
@@ -1,9 +1,3 @@
-//
-//  CartyTradPlusRewardedAdapter.m
-//  CartyTradPlus
-//
-//  Created by GZTD-03-01959 on 2026/1/12.
-//
 
 #import "CartyTradPlusRewardedAdapter.h"
 
@@ -20,7 +14,6 @@
     NSString *pid = item.config[@"pid"];
     if(appid == nil || pid == nil)
     {
-        //配置错误
         [self AdConfigError];
         return;
     }
